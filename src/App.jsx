e4e44bc32c38e3bb2e76a682b681bbb650285430
Tomoe-12 <li8993han@gmail.com>
2024-08-02 22:15:34 +0630

    project setup , navbar , hero , highlight done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from './components/Navbar.jsx'
+import Hero from './components/Hero.jsx'
+import Highlight from './components/HighLights.jsx'
+const App = () => {
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlight />
+    </main>
+  )
+}
+
+export default App
